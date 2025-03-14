087321d968d2971bb84f133d1dfe5bf12d68815e
sebavi1 <sebastian.avila7277@gmail.com>
2025-03-14 18:21:04 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    showTab('home');
-});
-function showTab(tabId) {
-    document.querySelectorAll('.tab-content').forEach(tab => {
-        tab.classList.remove('active-tab');
-    });
-    document.getElementById(tabId).classList.add('active-tab');
-}
