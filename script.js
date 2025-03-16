544644b2d42b6375cfb25fdc40403f07cb21da01
sebavi1 <sebastian.avila7277@gmail.com>
2025-03-16 00:36:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     showTab('home');
 });
-
 function showTab(tabId) {
     document.querySelectorAll('.tab-content').forEach(tab => {
         tab.classList.remove('active-tab');
